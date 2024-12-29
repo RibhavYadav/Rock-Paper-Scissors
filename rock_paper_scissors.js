7195efa231c4e90187e7af59680518f1c8e1575a
draby2466 <ribhavyadav2466@gmail.com>
2024-12-29 19:37:52 +0530

    Code to simulate one round of rock, papers and scissors.

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -4,7 +4,7 @@ function getComputerChoice() {
         case 0:
             return "rock";
         case 1:
-            return "paper-scissors";
+            return "paper";
         case 2:
             return "scissors";
     }
@@ -13,3 +13,38 @@ function getComputerChoice() {
 function getHumanChoice() {
     return prompt("Rock, paper or scissors?").toLowerCase();
 }
+
+function playRound(computerChoice, humanChoice) {
+    if (computerChoice === humanChoice) {
+        console.log("Draw!");
+        return -1;
+    }
+    if (computerChoice === "rock") {
+        if (humanChoice === "scissors") {
+            console.log("Computer wins the round!");
+            return 0;
+        } else if (humanChoice === "paper") {
+            console.log("Player wins the round!");
+            return 1;
+        }
+    } else if (computerChoice === "paper") {
+        if (humanChoice === "scissors") {
+            console.log("Player wins the round!");
+            return 1;
+        } else if (humanChoice === "rock") {
+            console.log("Computer wins the round!");
+            return 0;
+        }
+    } else {
+        if (humanChoice === "rock") {
+            console.log("Player wins the round!");
+            return 1;
+        } else if (humanChoice === "paper") {
+            console.log("Computer wins the round!");
+            return 0;
+        }
+    }
+}
+
+let humanScore = 0, computerScore = 0;
+result = playRound(getComputerChoice(), getHumanChoice());
